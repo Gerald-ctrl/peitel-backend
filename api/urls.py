69dd5c52777409aa119f96d078519216800a9676
Gerald-ctrl <fgaobsequio.student@ua.edu.ph>
2025-11-06 14:25:55 +0800

    first commit

---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('users/', views.get_users, name='get_users'),
+    path('users/<int:pk>/', views.user_detail, name='user_detail'),
+]
